19cbbbd5050374bc032ea6ee9eb7173acb852712
Steven Moxley <stevenjmoxley@gmail.com>
2017-02-07 13:36:50 -0500

    My python works going onto git

---
+++ ex3.py
@@ -0,0 +1,28 @@
+#prints a sentance to the screen
+print "I will now count my chickens."
+
+#prints the words Hens and Roosters as well as calcuating their totals
+print "Hens" , 25 + 30 / 6
+print "Roosters" , 100 - 25 * 3 % 4
+
+#another sentance
+print "Now I will count the eggs"
+
+#math calcuating that I don't fully understand :D
+print 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+#3 + 2 + 1 - 5 + 0 - .25 + 6
+
+print "Is it true that 3 + 2 < 5 - 7?"
+
+print 3 + 2 < 5 - 7
+
+print "What is 3 + 2?" , 3 + 2
+print "What is 5 - 7?" , 5 - 7
+
+print "Oh, that's why it's False."
+
+print "How about some more."
+
+print "Is it greater?", 5 > -2
+print "Is it greater or equal?", 5 >= -2
+print "Is it less or equal?", 5 <= -2
